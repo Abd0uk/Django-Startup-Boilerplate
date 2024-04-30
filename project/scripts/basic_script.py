a0c61c98eb9533a5b09830827bb4cb0db54e7b31
Abd0uk <azkotb@gmail.com>
2024-04-30 10:47:46 +0300

    Initial

---
+++ project/scripts/basic_script.py
@@ -0,0 +1,16 @@
+
+#! Import models here or any other parts of your django project to use in these scripts.
+#! This file must implement a run() function. This is what gets called when you run the script. 
+
+#? Example of the script
+# from AppName.models import ModelName
+
+# def run():
+#     questions = ModelName.objects.all()
+#     questions.delete()
+
+#? Example of the Usage
+
+#* python manage.py runscript ScriptName (Without .py extension)
+
+#! For more info check >> https://django-extensions.readthedocs.io/en/latest/runscript.html 
